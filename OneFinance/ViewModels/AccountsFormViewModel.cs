c0f7cdf3da01a016a4090efe8d14ce38b0a398dd
James Mata <jamesmata556@gmail.com>
2025-12-15 20:23:01 -0500

    First Phase of accounts page

---
+++ OneFinance/ViewModels/AccountsFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using OneFinance.Models;
+using OneFinance.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace OneFinance.ViewModels
+{
+    public partial class AccountsFormViewModel : ViewModelBase
+    {
+        private readonly IDatabaseService _databaseService;
+        private readonly INavigationService _navigationService;
+
+        public AccountsFormViewModel(IDatabaseService databaseService, INavigationService navigationService)
+        {
+
+        }
+    }
+}
---
+++ OneFinance/Views/AccountsFormView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OneFinance;
+
+public partial class AccountsFormView : UserControl
+{
+    public AccountsFormView()
+    {
+        InitializeComponent();
+    }
+}
